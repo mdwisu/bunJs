45d5d2327158f941f298c1ef5e0b647135b44590
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-01-13 08:59:45 +0700

    🚀 quickstart

--- index.tsx
+++ index.tsx
@@ -1,7 +1,15 @@
+import figlet from "figlet";
+
+import { hello } from "./hello";
+
+hello();
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("Bun!");
+    console.log(Bun.version);
+    const body = figlet.textSync("Bun!");
+
+    return new Response(body);
   },
 });
 
